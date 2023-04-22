beb6e9114dd6ca6ec5b694d0821c3017d9072f51
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2023-04-22 08:00:53 +0100

    printf project header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stddef.h>
+
+#endif
