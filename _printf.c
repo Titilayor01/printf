7ccc6b170ca7019fed79da62e605b54bf763388f
Titilayor01 <oyedeletitilayov@gmail.com>
2023-04-25 06:52:51 -0700

    finally!

--- _printf.c
+++ _printf.c
@@ -1,66 +1,35 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - function that replicate the printf function
+ * @format: format parameter
+ * @...: variable parameter
+ *
+ * Return: interger
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	unsigned int a, r_val = 0;
+	va_list vargs;
 
-	if (format == NULL)
-		return (-1);
+	va_start(vargs, format);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (a = 0; format[a] != '\0'; a++)
 	{
-		if (format[i] != '%')
+		if (format == NULL)
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			return (-1);
 		}
-		else
+		else if (format[a] != '%')
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			putchar(format[a]);
 		}
+		else if (format[a + 1] == 'c')
+		{
+			putchar(va_arg(vargs, int));
+			a++;
+		}
+	r_val++;
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	va_end(vargs);
+	return (r_val);
 }
